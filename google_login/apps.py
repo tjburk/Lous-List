8cc25113f4d09db3afd5f492bb4fa1138e2f3bfd
tjburk <ty_burkhardt@verizon.net>
2022-10-07 19:26:17 -0400

    Added google_login

--- google_login/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GoogleLoginConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'google_login'
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Runmanage.py migrate administrative tasks."""
+    """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lous_list.settings")
     try:
         from django.core.management import execute_from_command_line
