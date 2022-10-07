58e2e17ec730d74b44d6cd42b060a1c839023501
Gray <leofficialk1ng@gmail.com>
2022-10-07 18:05:43 -0400

    Deleted unnecessary Views

--- lous_list/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
-from django.utils import timezone
-
-class loginView(generic.DetailView):
-    template_name='lous_list/login.html'
