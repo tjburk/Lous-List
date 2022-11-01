71a4c313ab63b2ac8ac725d68b044c741af8d835
tjburk <ty_burkhardt@verizon.net>
2022-10-31 23:13:25 -0400

    Integrated newly migrated comments app

--- list_classes/urls.py
+++ list_classes/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'list_classes'
 urlpatterns = [
-    path('<str:subjects_displayed>/',views.IndexView.as_view(),name="classes"),
+    path('<str:subjects_displayed>/', views.IndexView.as_view(), name="classes"),
     path('description/<int:course_number>/', views.description, name='description'),
     path('update/',views.update_course_db,name="update"),
 ]
