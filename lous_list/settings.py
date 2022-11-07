2636ab47386e6879cda546668a62ed8406d9f4f9
Gray <leofficialk1ng@gmail.com>
2022-11-07 14:44:26 -0500

    Whitenoise to settings

--- lous_list/settings.py
+++ lous_list/settings.py
@@ -55,12 +55,14 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
 ]
 
 ROOT_URLCONF = "lous_list.urls"
