43ea12ffe0f9c0c1a808f3f96746a652f2c67403
Gray <leofficialk1ng@gmail.com>
2022-11-29 17:30:48 -0500

    Logic for overlapping courses

--- schedule/views.py
+++ schedule/views.py
@@ -3,6 +3,7 @@ from list_classes.models import Course
 from django.contrib import messages
 from schedule.models import Schedule
 from django.contrib.auth.models import User
+import numpy as np
 
 
 def get_schedule_courses(request, user_id):
@@ -39,8 +40,45 @@ def add_course_to_schedule(request, course_number):
         messages.warning(request, "Course already in schedule.")
         # Refresh current page
         return redirect(request.META.get('HTTP_REFERER'))
+    
     # If course times overlap
+    course_days=course.meetings_days
+    course_time_start=course.meetings_start_time
+    course_time_end=course.meetings_end_time
 
+    course_list_query = user.schedule.courses.all()
+    for course_list in course_list_query:
+        course_in_list = Course.objects.get(course_number=course_list.course_number)
+        if((course_in_list.meetings_days in course_days) or (course_days in course_in_list.meetings_days)):
+            listCourse_time_start=course_in_list.meetings_start_time
+            listCourse_time_end=course_in_list.meetings_end_time
+            
+            temp=3
+            a=float(course_time_start[0:2]+"."+course_time_start[3:5])
+            b=float(course_time_end[0:2]+"."+course_time_end[3:5])
+
+            c=float(listCourse_time_start[0:2]+"."+listCourse_time_start[3:5])
+            d=float(listCourse_time_end[0:2]+"."+listCourse_time_end[3:5])
+
+            timeList1=[]
+            for i in np.arange(a,b,.01):
+                temp=format(i,'.2f')
+                timeList1.append(temp)
+
+            timeList2=[]
+            for i in np.arange(c,d,.01):
+                temp=format(i,'.2f')
+                timeList2.append(temp)
+            result=False
+            for x in timeList1:
+                for y in timeList2:
+                    if x==y:
+                        result=True
+            if result:
+                messages.warning(request, "Course has overlap with course added to schedule.")
+                # Refresh current page
+                return redirect(request.META.get('HTTP_REFERER'))
+            
     # Everything is good! Add course to schedule
     else:
         current_schedule.add_course(course)
