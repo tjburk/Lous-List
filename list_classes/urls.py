9f1ba49808fa21e5ca72725ab7df2d04c3674d89
tjburk <ty_burkhardt@verizon.net>
2022-12-06 18:14:43 -0500

    Can now enroll in components

--- list_classes/urls.py
+++ list_classes/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('comment/', include('comment.urls')),
     path('<str:subjects_displayed>/', views.IndexView.as_view(), name="classes"),
     path('description/<int:course_number>/', views.description, name='description'),
+    #path('description/<int:course_number>/<int:component_number>/', views.description_component, name='description_component'),
 ]
